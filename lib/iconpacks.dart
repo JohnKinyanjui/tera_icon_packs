4ad169bfdb3f272e0540e6623f51335dc20afbae
Johnkinyanjui <johnkinyanjuiwanjiru@gmail.com>
2021-12-09 15:29:56 +0300

    init

---
+++ lib/iconpacks.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+class IconPacks {
+  static const MethodChannel _channel = const MethodChannel('iconpacks');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  static Future<List> get iconPacks async {
+    String packs = await _channel.invokeMethod("getIconPacks");
+    packs = packs.replaceAll("[", "").replaceAll("]", "").replaceAll(" ", "");
+    return packs.split(",");
+  }
+
+  static Future<Image?> getIcon(String packageName, String iconPackPackage,
+      {double width = 50, double height = 50}) async {
+    var args = {'application': packageName, 'icon_pack': iconPackPackage};
+    var val = await _channel.invokeMethod("getIcon", args);
+    if (val is String) return null;
+    Uint8List list = val;
+    return Image.memory(
+      list,
+      width: width,
+      height: height,
+      fit: BoxFit.contain,
+    );
+  }
+}
